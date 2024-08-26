7629cda4e19198e1d32f9005b7efa4fab69b6d66
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-08-26 16:42:45 -0300

    feat: add screen SignUp

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import {
 import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Loading } from './src/components/Loading'
-import SignIn from './src/screens/SignIn'
+import { SignUp } from './src/screens/SignUp'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
@@ -21,7 +21,7 @@ export default function App() {
         translucent
       />
       {!fontsLoaded && <Loading />}
-      {fontsLoaded && <SignIn />}
+      {fontsLoaded && <SignUp />}
     </GluestackUIProvider>
   )
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -11,7 +11,7 @@ import Logo from '@assets/logo.svg'
 import { Input } from '@components/Input'
 import Button from '@components/Button'
 
-export function SignIn() {
+export function SignUp() {
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
@@ -33,23 +33,19 @@ export function SignIn() {
               Treine sua mente e o seu corpo.
             </Text>
           </Center>
-          <Center gap="$2">
-            <Heading color="$gray100">Acesse a conta.</Heading>
+          <Center gap="$2" flex={1}>
+            <Heading color="$gray100">Crie sua conta.</Heading>
             <Input
               placeholder="E-mail"
               keyboardType="email-address"
               autoCapitalize="none"
             />
+            <Input placeholder="Nome:" />
             <Input placeholder="Senha" secureTextEntry />
-            <Button title="Acessar" />
+            <Button title="Criar e acessar" />
           </Center>
 
-          <Center flex={1} justifyContent="flex-end" mt="$4">
-            <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
-              Ainda não tem acesso?
-            </Text>
-            <Button title="Criar Conta" variant="outline" />
-          </Center>
+          <Button title="Voltar para o login" variant="outline" mt="$12" />
         </VStack>
       </VStack>
     </ScrollView>
