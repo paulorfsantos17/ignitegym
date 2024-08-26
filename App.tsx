cf1ab86df698ebbcec7d28aa09c1b4316e8dabe7
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-08-26 14:39:19 -0300

    feat: add  config theme

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
-import { Text, View, StatusBar } from "react-native";
+import {View, StatusBar } from "react-native";
 import {useFonts, Roboto_700Bold, Roboto_400Regular} from "@expo-google-fonts/roboto"
+import { Center, GluestackUIProvider, Text } from "@gluestack-ui/themed";
+import { config } from './config/gluestack-ui.config'
 
 
 
@@ -8,12 +10,14 @@ export default function App() {
   const [fontsLoaded] = useFonts({Roboto_700Bold, Roboto_400Regular})
 
   if(!fontsLoaded){
-    return <View></View>
+    return <Center></Center>
   }
   return (
-    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+    <GluestackUIProvider config={config}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+      <Center flex={1} bg="$gray600">
+        <Text color="white">Open up App.tsx to start working on your app!</Text>
+      </Center>
+    </GluestackUIProvider>
   );
 }
