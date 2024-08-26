6086e641e88788af4881c2a45357fad365513332
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-08-26 14:09:09 -0300

    feat: create project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
+import {useFonts, Roboto_700Bold, Roboto_400Regular} from "@expo-google-fonts/roboto"
+
+
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_700Bold, Roboto_400Regular})
+
+  if(!fontsLoaded){
+    return <View></View>
+  }
   return (
-    <View style={styles.container}>
+    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
