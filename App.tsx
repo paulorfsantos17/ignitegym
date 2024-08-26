4f3878ec415ce2b29653f3595df0c75178c2d609
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-08-26 15:03:09 -0300

    chore: config eslint

--- App.tsx
+++ App.tsx
@@ -1,26 +1,32 @@
-import { StatusBar } from "react-native";
-import {useFonts, Roboto_700Bold, Roboto_400Regular} from "@expo-google-fonts/roboto"
-import { Center, GluestackUIProvider, Text } from "@gluestack-ui/themed";
+/* eslint-disable camelcase */
+import { StatusBar } from 'react-native'
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from '@expo-google-fonts/roboto'
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
-import {Loading} from "./src/components/Loading";
-
-
-
+import { Loading } from './src/components/Loading'
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Roboto_700Bold, Roboto_400Regular})
-
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
 
   return (
     <GluestackUIProvider config={config}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      {!fontsLoaded && <Loading />  }
-      {fontsLoaded && 
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {!fontsLoaded && <Loading />}
+      {fontsLoaded && (
         <Center flex={1} bgColor="$gray700">
-          <Text color="white">Open up App.tsx to start working on your app!</Text>
+          <Text color="white">
+            Open up App.tsx to start working on your app!
+          </Text>
         </Center>
-      }
-      
+      )}
     </GluestackUIProvider>
-  );
+  )
 }
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,9 @@
-import { Center, Spinner } from "@gluestack-ui/themed";
+import { Center, Spinner } from '@gluestack-ui/themed'
 
-
-export  function Loading() {
+export function Loading() {
   return (
-    <Center flex={1} bg="$gray700" >
-      <Spinner color="green500"/>
+    <Center flex={1} bg="$gray700">
+      <Spinner color="green500" />
     </Center>
   )
 }
