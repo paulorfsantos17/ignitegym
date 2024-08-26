b96e4beaf7bc8a90c18b3aa52700cb27369c8d71
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-08-26 16:35:38 -0300

    feat: add screen SignIn

--- App.tsx
+++ App.tsx
@@ -5,9 +5,10 @@ import {
   Roboto_700Bold,
   Roboto_400Regular,
 } from '@expo-google-fonts/roboto'
-import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed'
+import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Loading } from './src/components/Loading'
+import SignIn from './src/screens/SignIn'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
@@ -20,13 +21,7 @@ export default function App() {
         translucent
       />
       {!fontsLoaded && <Loading />}
-      {fontsLoaded && (
-        <Center flex={1} bgColor="$gray700">
-          <Text color="white">
-            Open up App.tsx to start working on your app!
-          </Text>
-        </Center>
-      )}
+      {fontsLoaded && <SignIn />}
     </GluestackUIProvider>
   )
 }
