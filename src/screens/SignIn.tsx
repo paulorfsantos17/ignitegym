75033f8d5761a7f0a8e854ec74d94466616af396
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-08-26 15:28:53 -0300

    chore: config use SVG file for expo

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,17 @@
+import { Image, VStack } from '@gluestack-ui/themed'
+import BackgroundImg from '@assets/background.png'
+
+export default function SignIn() {
+  return (
+    <VStack flex={1} bg="$gray700">
+      <Image
+        w="$full"
+        h={624}
+        source={BackgroundImg}
+        defaultSource={BackgroundImg}
+        alt="Pessoa Treinando"
+        position="absolute"
+      />
+    </VStack>
+  )
+}
