91b26231cf794989ece1813a642a7618cb0636db
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-08-26 16:31:30 -0300

    style: :art: add variant outline in Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,29 +8,34 @@ import type { ComponentProps } from 'react'
 interface ButtonProps extends ComponentProps<typeof GluestackButton> {
   title: string
   isLoading?: boolean
+  variant?: 'solid' | 'outline'
 }
 
 export default function Button({
   title,
   isLoading = false,
+  variant = 'solid',
   ...rest
 }: ButtonProps) {
   return (
     <GluestackButton
       w="$full"
       h="$14"
-      bg="$green700"
-      borderWidth="$0"
+      bg={variant === 'outline' ? 'transparent' : '$green700'}
+      borderWidth={variant === 'outline' ? '$1' : '$0'}
       borderColor="$green500"
       rounded="$sm"
-      $active-bg="$green500"
+      $active-bg={variant === 'outline' ? '$gray500' : '$green500'}
       disabled={isLoading}
       {...rest}
     >
       {isLoading ? (
         <ButtonSpinner />
       ) : (
-        <Text color="$white" fontWeight="bold">
+        <Text
+          color={variant === 'outline' ? '$green500' : '$white'}
+          fontWeight="bold"
+        >
           {title}
         </Text>
       )}
