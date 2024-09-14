8b7d166dffa279b29e401deccbacc0d3cb328280
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-14 15:23:31 -0300

    feat: add Group

---
+++ src/components/Group.tsx
@@ -0,0 +1,35 @@
+import { Button, Text } from '@gluestack-ui/themed'
+import { ComponentProps } from 'react'
+
+type GroupProps = ComponentProps<typeof Button> & {
+  name: string
+  isActive?: boolean
+}
+export default function Group({ name, isActive = false, ...rest }: GroupProps) {
+  return (
+    <Button
+      minWidth="$24"
+      h="$10"
+      bg="$gray600"
+      rounded="$md"
+      alignItems="center"
+      borderColor="$green500"
+      borderWidth={isActive ? 1 : 0}
+      sx={{
+        ':active': {
+          borderWidth: 1,
+        },
+      }}
+      {...rest}
+    >
+      <Text
+        color={isActive ? '$green500' : 'gray200'}
+        textTransform="uppercase"
+        fontSize="$xs"
+        fontFamily="$heading"
+      >
+        {name}
+      </Text>
+    </Button>
+  )
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import Group from '@components/Group'
 import HomeHeader from '@components/HomeHeader'
 import { VStack } from '@gluestack-ui/themed'
 
@@ -5,6 +6,7 @@ export function Home() {
   return (
     <VStack>
       <HomeHeader />
+      <Group name="Biceps" isActive={true} />
     </VStack>
   )
 }
