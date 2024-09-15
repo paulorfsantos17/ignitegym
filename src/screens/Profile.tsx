5040d16e55005f1e4b0d5b95196742f22583d363
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-15 15:33:13 -0300

    feat: add userPhoto in profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,21 @@
 import ScreenHeader from '@components/ScreenHeader'
-import { VStack } from '@gluestack-ui/themed'
+import UserPhoto from '@components/UserPhoto'
+import { Center, VStack } from '@gluestack-ui/themed'
+import { ScrollView } from 'react-native'
 
 export function Profile() {
   return (
     <VStack>
       <ScreenHeader title="Perfil" />
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <UserPhoto
+            source={{ uri: 'https:github.com/paulorfsantos17.png' }}
+            alt="Imagem de perfil"
+            size="xl"
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
