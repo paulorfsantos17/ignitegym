d0d70776dfa1a49c2f5633c9933954bf77ae8279
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-16 13:57:32 -0300

    feat: ensure get image gallery

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -3,20 +3,58 @@ import { Input } from '@components/Input'
 import ScreenHeader from '@components/ScreenHeader'
 import UserPhoto from '@components/UserPhoto'
 import { Center, Heading, Text, VStack } from '@gluestack-ui/themed'
-import { ScrollView, TouchableOpacity } from 'react-native'
+import * as FileSystem from 'expo-file-system'
+import * as ImagePicker from 'expo-image-picker'
+import { useState } from 'react'
+import { Alert, ScrollView, TouchableOpacity } from 'react-native'
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState(
+    'https:github.com/paulorfsantos17.png',
+  )
+
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
+
+      if (photoSelected.canceled) {
+        return false
+      }
+
+      const photoURI = photoSelected.assets[0].uri
+
+      if (photoURI) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoURI)) as {
+          size: number
+        }
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return Alert.alert(
+            'Está imagem é muito grande.Escolha uma de até 5MB.',
+          )
+        }
+        setUserPhoto(photoURI)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
-            source={{ uri: 'https:github.com/paulorfsantos17.png' }}
+            source={{ uri: userPhoto }}
             alt="Imagem de perfil"
             size="xl"
           />
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="$green500"
               fontFamily="$heading"
