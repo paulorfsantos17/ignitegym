419d7ba54d321228b76a23a85e51a26fe330e1c6
Paulo Ricardo Santos <42043880+paulorfsantos17@users.noreply.github.com>
2024-09-19 16:22:25 -0300

    feat: ensure save and get user in storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,13 @@
 import { api } from '@services/api'
-import { createContext, type ReactNode, useState } from 'react'
+import { storageUserGet, storageUserSave } from '@storage/storageUser'
+import { AppError } from '@utils/AppError'
+import { createContext, type ReactNode, useEffect, useState } from 'react'
 
 import { UserDTO } from '../dtos/UserDTO'
 export interface AuthContextDataProps {
   user: UserDTO
   signIn: (email: string, password: string) => Promise<void>
+  isLoadingUserStorageData: boolean
 }
 interface AuthContextProviderProps {
   children: ReactNode
@@ -14,16 +17,36 @@ export const AuthContext = createContext({} as AuthContextDataProps)
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO)
+  const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true)
 
   async function signIn(email: string, password: string) {
     const { data } = await api.post('/sessions', { email, password })
     if (data.user) {
       setUser(data.user)
+      storageUserSave(data.user)
     }
   }
 
+  async function loadUserData() {
+    try {
+      const userLogged = await storageUserGet()
+      if (userLogged.id) {
+        setIsLoadingUserStorageData(false)
+        setUser(userLogged)
+      }
+    } catch (error) {
+      throw new AppError(
+        'Erro ao carregar usuário. tente novamente mais tarde.',
+      )
+    }
+  }
+
+  useEffect(() => {
+    loadUserData()
+  }, [])
+
   return (
-    <AuthContext.Provider value={{ user, signIn }}>
+    <AuthContext.Provider value={{ user, signIn, isLoadingUserStorageData }}>
       {children}
     </AuthContext.Provider>
   )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Loading } from '@components/Loading'
 import { Box } from '@gluestack-ui/themed'
 import { useAuth } from '@hooks/useAuth'
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
@@ -7,9 +8,12 @@ import { AppRoutes } from './app.routes'
 import AuthRoutes from './auth.routes'
 
 export function Routes() {
-  const { user } = useAuth()
+  const { user, isLoadingUserStorageData } = useAuth()
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
+  if (isLoadingUserStorageData) {
+    return <Loading />
+  }
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
